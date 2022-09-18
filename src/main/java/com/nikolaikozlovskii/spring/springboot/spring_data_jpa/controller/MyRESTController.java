3acfbe22ef23ba4492183a609cc47cfa89f6c62a
Nikolai <nik-neman@mail.ru>
2022-09-18 11:42:37 +0200

    stady metods JpaRepository

--- src/main/java/com/nikolaikozlovskii/spring/springboot/spring_data_jpa/controller/MyRESTController.java
+++ src/main/java/com/nikolaikozlovskii/spring/springboot/spring_data_jpa/controller/MyRESTController.java
@@ -40,9 +40,15 @@ public class MyRESTController {
 
     @DeleteMapping("/employees/{id}")
     public String deleteEmployee(@PathVariable int id){
-        Employee employee = employeeService.getEmployee(id);
+       employeeService.deleteEmployee(id);
 
             return "Employee with ID = " + id + " was deleted";
 
+    }
+
+    @GetMapping("/employees/name/{name}")
+    public List<Employee> showAllEmployeesByName(@PathVariable String name){
+        List<Employee> employees = employeeService.findAllByName(name);
+        return employees;
     }
 }
--- src/main/java/com/nikolaikozlovskii/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
+++ src/main/java/com/nikolaikozlovskii/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-  
+  //Можем добавить недостающие методы(имена задавать по подсказке IDE
+    public List<Employee> findAllByName(String name);
 }
